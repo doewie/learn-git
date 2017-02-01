bd77f0e3cb52da64f886de860572f5fe31c6e553
doewie <douwe@laranja.nl>
2017-02-01 09:10:43 +0000

    remove invalid includes

--- always.fs
+++ always.fs
@@ -4,12 +4,6 @@
 $5000 eraseflashfrom
 ( basic definitions, compiled to flash )
 compiletoflash
-
-\	include common stuff
-include ../../common/registers_memory_map.fs
-include ../../common/io.fs 
-include ../../common/base.fs
-include ../../common/doewie.fs
   
 ( define cornerstone for eraseflash )
 cornerstone eraseflash
